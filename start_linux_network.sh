d84193087526a528255f5e62efca4bf6739f2f60
secf00tprint <secf00tprint@gmail.com>
2020-06-27 09:29:24 +0200

    add check for docker group without you have to use sudo

--- start_linux_network.sh
+++ start_linux_network.sh
@@ -2,6 +2,12 @@
 
 DOCKER_COMPOSE_FILE=docker-compose.yml
 
+if ! [[ "$(groups)" =~ (^|[[:space:]])docker($|[[:space:]]) ]]
+then
+    echo "You not to be root to run this application"
+    exit
+fi
+
 # Check if docker, docker-machine and docker-compse is installed
 command -v docker >/dev/null 2>&1 || { echo >&2 "docker needed (see https://www.docker.com)"; exit 1; }
 command -v docker-machine >/dev/null 2>&1 || { echo >&2 "docker-machine needed (see https://docs.docker.com/machine/install-machine/)"; exit 1; }
