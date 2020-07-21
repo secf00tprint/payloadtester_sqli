138111b37f709c5d1f2a4589a10f824ac0f2686e
secf00tprint <secf00tprint@gmail.com>
2020-07-21 11:47:19 +0200

    remove unnecessary code

--- start_linux_network.sh
+++ start_linux_network.sh
@@ -20,12 +20,5 @@ function cleanup {
 }
 trap cleanup EXIT
 
-# Set linux docker
-eval $(docker-machine env -unset)
-
-if [ -n "$DOCKER_VM_IP" ]; then
-    unset DOCKER_VM_IP
-fi
-
 # Start network
 docker-compose -f $DOCKER_COMPOSE_FILE up --build
