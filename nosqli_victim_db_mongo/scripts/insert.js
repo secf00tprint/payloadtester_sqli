463c0a063add56d2275a2151a9b92969bb8ccbd5
secf00tprint <secf00tprint@gmail.com>
2020-07-25 21:52:27 +0200

    make more realistic price values

---
+++ nosqli_victim_db_mongo/scripts/insert.js
@@ -0,0 +1,12 @@
+// Init nosqli db
+db.products.insertOne({product:"Product A", price: "26"});
+db.products.insertOne({product:"Product B", price: "9"});
+db.products.insertOne({product:"Product C", price: "34"});
+db.products.insertOne({product:"Product D", price: "81"});
+db.products.insertOne({product:"Product E", price: "68"});
+
+db.users.insertOne({"user":"admin", "password":"Eethe5Yee7aephe"});
+db.users.insertOne({"user":"bernice", "password":"bieth0rieH6ohth"});
+db.users.insertOne({"user":"dolly", "password":"wiC6aeloZ1aexee"});
+db.users.insertOne({"user":"william", "password":"Oizai9ieJe9Ietu"});
+db.users.insertOne({"user":"kory", "password":"biesuR8coongeih"});
