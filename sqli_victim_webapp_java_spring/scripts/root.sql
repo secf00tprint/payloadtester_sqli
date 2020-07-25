c10e0eb44e89a215ee8065b8db1c8b7104565160
secf00tprint <secf00tprint@gmail.com>
2020-07-25 22:13:45 +0200

    rename sql -> sqli

--- sqli_victim_webapp_java_spring/scripts/root.sql
+++ sqli_victim_webapp_java_spring/scripts/root.sql
@@ -1,5 +1,2 @@
 FLUSH PRIVILEGES;
 update mysql.user set password=password('rootpwd') where user='root';
--- FLUSH PRIVILEGES;
--- update mysql.user set plugin='mysql_native_password' where user='root';
--- FLUSH PRIVILEGES;
