b54a0bb220f1f6dff1f0fd1cd85057dd6a78cb66
secf00tprint <secf00tprint@gmail.com>
2020-07-25 22:49:18 +0200

    add debug output to logs

--- sqli_victim_webapp_java_spring/scripts/startup.sql
+++ sqli_victim_webapp_java_spring/scripts/startup.sql
@@ -1,8 +1,3 @@
--- update mysql.user set password=password('rootpwd') where user='root';
--- FLUSH PRIVILEGES;
--- update mysql.user set plugin='mysql_native_password' where user='root';
--- FLUSH PRIVILEGES;
--- SET PASSWORD = PASSWORD('rootpwd');
 CREATE OR REPLACE USER 'user'@'localhost' IDENTIFIED BY 'password';
 -- ****************************
 -- SQL Injection example
