3bf7f0865324ce1f0b36ecc9cdf8f30789b75c28
secf00tprint <secf00tprint@gmail.com>
2021-01-26 14:09:27 +0100

    add initial example data

--- sqli_victim_webapp_java/scripts/startup.sql
+++ sqli_victim_webapp_java/scripts/startup.sql
@@ -5,3 +5,14 @@ CREATE OR REPLACE USER 'user'@'localhost' IDENTIFIED BY 'password';
 CREATE DATABASE `sqli_example`;
 GRANT ALL PRIVILEGES ON `sqli_example`.* TO 'user'@'localhost'; 
 GRANT FILE           ON *.*           TO 'user'@'localhost';
+
+-- Fill with example data
+USE `sqli_example`;
+CREATE TABLE `user` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`)
+                    );
+INSERT INTO `user` (`username`,`password`) VALUES ('admin','4dm1n');
+INSERT INTO `user` (`username`,`password`) VALUES ('user123','pass');
